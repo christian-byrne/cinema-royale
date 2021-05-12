a35a5971d3d458b4af5b334a4a56c4f881aa7730
trevor-reznik <i@bymyself.life>
2021-05-12 16:40:58 -0700

    init

---
+++ js/answerKey.js
@@ -0,0 +1,130 @@
+
+var answerKey = { 
+
+// #endregion --------------------------------------------------------------
+// #region                Video 1 -- 
+//-------------------------------------------------------------------------- 
+
+  "g": [0, 10], 
+  
+
+
+// #endregion --------------------------------------------------------------
+// #region                Video 2 -- 
+//-------------------------------------------------------------------------- 
+
+
+
+
+
+// #endregion --------------------------------------------------------------
+// #region                Video 3 -- Mega Shots 2 (My Cut)
+//-------------------------------------------------------------------------- 
+
+  "slumdog millionaire":
+
+
+
+// #endregion --------------------------------------------------------------
+// #region                Video 4 -- Mega Shots
+//-------------------------------------------------------------------------- 
+
+  "ferris bueller's": [0, 2.830865],
+  "Ferris Bueller's": [0, 2.830865],
+  "day off": [0, 2.830865],
+  "Day Off": [0, 2.830865],
+  "ferris buellers": [0, 2.830865],
+  "Ferris Buellers": [0, 2.830865],
+  "Ferris Buller's": [0, 2.830865],
+  "Ferris Bueler's": [0, 2.830865],
+
+  "shawshank redemption": [2.3, 6.942655],
+  "Shawshank Redemption": [2.3, 6.942655],
+  "Shawshank": [2.3, 6.942655],
+  "shawshank": [2.3, 6.942655],
+
+  // Bird's Nest Guy
+  "bird nest": [7, 11.030507],
+
+  "sin  city": [10.8, 13.917121],
+  "Sin City": [10.8, 13.917121],
+  "Sin": [10.8, 13.917121],
+
+  // Tapping Feet
+  "tapping feet laundry mat": [12.8, 16.270321],
+
+  "saving private ryan": [15.8, 19.057519],
+  "Saving Private": [15.8, 19.057519],
+  "Saving Private Ryan": [15.8, 19.057519],
+  "Private Ryan": [15.8, 19.057519],
+  "Ryan": [15.8, 19.057519],
+  
+  // Plane Burning
+  "burning": [18.8, 22.142927],
+
+  "Truman": [21, 25.224021],
+  "truman": [21, 25.224021],
+  "the truman show": [21, 25.224021],
+  "The Truman Show": [21, 25.224021],
+
+  "fight club": [23.8, 29.401947],
+  "Fight Club": [23.8, 29.401947],
+
+  "matrix": [28, 31.832617],
+  "Matrix": [28, 31.832617],
+
+  "Nightcrawler": [30, 34.646121],
+  "nightcrawler": [30, 34.646121],
+  "Night Crawler": [30, 34.646121],
+  "night crawler": [30, 34.646121],
+  "crawler": [30, 34.646121],
+
+  // Soldiers walking in desert
+  "solders": [32.5, 38.438925],
+
+  "raging bull": [36.7, 41.410634],
+  "Raging Bull": [36.7, 41.410634],
+  "Raging": [36.7, 41.410634],
+
+  "Mad Max": [39.6, 45.186213],
+  "max max": [39.6, 45.186213],
+  "Fury Road": [39.6, 45.186213],
+  "fury road": [39.6, 45.186213],
+
+  "Inglorious": [44, 46.840851],
+  "Basterds": [44, 46.840851],
+  "Bastards": [44, 46.840851],
+  "inglrious": [44, 46.840851],
+  "bastards": [44, 46.840851],
+  "basterds": [44, 46.840851],
+
+  "pyscho": [45, 49.697129],
+  "Pyscho": [45, 49.697129],
+
+  // Holding bloody hands in the forest
+  "holding": [48, 52.778584],
+
+  "full metal jacket": [50, 56.659916],
+  "Full Metal": [50, 56.659916],
+  "metal jacket": [50, 56.659916],
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
